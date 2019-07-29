c1b8ec62899ed9c7fd0b9349cbf9b2277a46a0c6
matsalen <k.kurosaki@hotmail.com>
2019-07-29 17:25:11 -0300

    roll a ball mobile adaptação

---
+++ RollABall/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] private float velocidade;
+
+    private Rigidbody rb;
+    private Vector3 forca;
+    private Vector3 movimento;
+    private float movimentoX;
+    private float movimentoY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        forca = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //movimentoX = Input.GetAxis("Horizontal");
+        //movimentoY = Input.GetAxis("Vertical");
+
+        movimento = Input.acceleration;
+
+        forca.x = movimento.x;
+        forca.z = movimento.y;
+        forca *= velocidade;
+        rb.AddForce(forca);
+    }
+}
